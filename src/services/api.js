736297da2588fd63ffc0025b0ebc4a6b4c1473ff
“Muhammad <ansarimuhammad2005@gmail.com>
2026-09-17 16:07:36 +0530

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -30,7 +30,7 @@
 //   return data;
 // }
 
-const API_BASE_URL = "https://127.0.0.1:5000"
+const API_BASE_URL = "http://127.0.0.1:5000"
 
 export async function checkBackendHealth() {
   const response = await fetch(`${API_BASE_URL}/`);
