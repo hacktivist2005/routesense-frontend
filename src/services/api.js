f12ca6823493298f4abeb4c723141dc43b6c9a31
“Muhammad <ansarimuhammad2005@gmail.com>
2026-09-17 11:56:34 +0530

    Backend Updated

--- src/services/api.js
+++ src/services/api.js
@@ -30,7 +30,7 @@
 //   return data;
 // }
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://routesense-backend-1up3.onrender.com";
 
 export async function checkBackendHealth() {
   const response = await fetch(`${API_BASE_URL}/`);
